e6d9241635bdad9e24bee9d613ce410b091d7ff1
carlosvts <carlosvtsdev@gmail.com>
2026-01-03 01:16:22 -0300

    add output stream functionality

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     if (argc < 3){
-        std::cout << "Usage: " << argv[0] << "<file> <effect>\n";
+        std::cout << "Usage: " << argv[0] << "<file> <effect> <output>\n";
         std::cout << "effects: gausianblur, grayscale, sepia, negative, threshold, edge";
         return 1;
     }
@@ -15,18 +15,19 @@ int main(int argc, char *argv[])
     BMP originalImage(imagePath);
     BMP newImage = originalImage;
     char *effectOption = argv[2];
+    const char *outputFileName = argv[3];
 
     if (strcmp(effectOption, filters[0]) == 0){
         for(int i = 0; i < 15; i++){
             newImage.applyGausianBlur();
         }
-        newImage.write("gausianFiltered.bmp");
+        newImage.write(outputFileName);
     }
-    else if (strcmp(effectOption, filters[1]) == 0) { newImage.toGrayScale();  newImage.write("grayFiltered.bmp"); }
-    else if (strcmp(effectOption, filters[2]) == 0) { newImage.toSepia(); newImage.write("sepiaFiltered.bmp"); }
-    else if (strcmp(effectOption, filters[3]) == 0) { newImage.toNegative(); newImage.write("negativeFiltered.bmp"); }
-    else if (strcmp(effectOption, filters[4]) == 0) { newImage.toThreshold(); newImage.write("thresholdFiltered.bmp"); }
-    else if (strcmp(effectOption, filters[5]) == 0) { newImage.sobelEdgeDetector(); newImage.write("sobelFiltered.bmp"); }
+    else if (strcmp(effectOption, filters[1]) == 0) { newImage.toGrayScale();  newImage.write(outputFileName); }
+    else if (strcmp(effectOption, filters[2]) == 0) { newImage.toSepia(); newImage.write(outputFileName); }
+    else if (strcmp(effectOption, filters[3]) == 0) { newImage.toNegative(); newImage.write(outputFileName); }
+    else if (strcmp(effectOption, filters[4]) == 0) { newImage.toThreshold(); newImage.write(outputFileName); }
+    else if (strcmp(effectOption, filters[5]) == 0) { newImage.sobelEdgeDetector(); newImage.write(outputFileName); }
     else { 
         std::cout << "Incorret usage, effects are case-sensitive!" << std::endl;    
         return 1; 
